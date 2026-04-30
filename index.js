bd0bb8598bd206354aade123743171de8284c34b
JamesRWri <ninjamez@byui.edu>
2026-04-29 20:42:58 -0600

    Added express routes and environment variables

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
+// set view engine
+app.set("view engine", "ejs");
+
+// static files
+app.use(express.static("public"));
+
+// routes
 app.get("/", (req, res) => {
-  res.send("Hello CSE 340!");
+  res.render("home", { title: "Home" });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", { title: "About" });
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact", { title: "Contact" });
 });
 
 app.listen(PORT, () => {
